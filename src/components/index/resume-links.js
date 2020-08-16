b60fabccf95364b4741acbdf0077bc450fcbc32d
Rakesh Patra <rakesh.patra@bigbinary.com>
2020-08-16 20:59:37 +0530

    Fixed data

--- src/components/index/resume-links.js
+++ src/components/index/resume-links.js
@@ -28,7 +28,7 @@ const ResumeLinks = () => {
       </GatsbyLink>
       <Link
         variant="resume"
-        href="https://github.com/rakeshpatra/website/raw/master/files/resume.pdf"
+        href="https://github.com/rakeshpatra/rakeshpatra.github.io/raw/master/files/resume.pdf"
         target="_blank"
         marginLeft={3}
       >
